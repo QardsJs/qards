90c05190a6f9e5182585b6e930b9c2ef3b276985
Romeo Mihalcea <romeo.mihalcea@gmail.com>
2018-09-13 11:05:50 +0300

    Possible fix for netlify deployments

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,14 @@
 const _ = require('lodash');
 const Promise = require('bluebird');
 const path = require('path');
-const {createFilePath} = require(`gatsby-source-filesystem`);
-const {createRemoteFileNode} = require(`gatsby-source-filesystem`);
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const { createRemoteFileNode } = require(`gatsby-source-filesystem`);
 
 const getTags = (edges) => {
 	const tags = [];
 
-	_.each(edges, edge => {
-		if (_.get(edge, "node.tags")) {
+	_.each(edges, (edge) => {
+		if (_.get(edge, 'node.tags')) {
 			for (let i = 0; i < edge.node.tags.length; i++) {
 				tags.push(edge.node.tags[i]);
 			}
@@ -21,8 +21,8 @@ const getTags = (edges) => {
 const getCategories = (edges) => {
 	const categories = [];
 
-	_.each(edges, edge => {
-		if (_.get(edge, "node.categories")) {
+	_.each(edges, (edge) => {
+		if (_.get(edge, 'node.categories')) {
 			for (let i = 0; i < edge.node.categories.length; i++) {
 				categories.push(edge.node.categories[i]);
 			}
@@ -32,15 +32,16 @@ const getCategories = (edges) => {
 	return categories;
 };
 
-exports.createPages = ({graphql, actions}) => {
-	const {createPage} = actions;
+exports.createPages = ({ graphql, actions }) => {
+	const { createPage } = actions;
 
 	return new Promise((resolve, reject) => {
 		const postTemplate = path.resolve('./src/templates/post/index.tsx');
-		const tagTemplate = path.resolve("./src/templates/tag/index.tsx");
-		const categoryTemplate = path.resolve("./src/templates/category/index.tsx");
+		const tagTemplate = path.resolve('./src/templates/tag/index.tsx');
+		const categoryTemplate = path.resolve('./src/templates/category/index.tsx');
 
-		resolve(graphql(`
+		resolve(
+			graphql(`
 				{
 					allContentfulPost(sort: {fields: [createdAt], order: DESC}) {
 						edges {
@@ -66,57 +67,61 @@ exports.createPages = ({graphql, actions}) => {
 						}
 					}
 				}
-			`).then(result => {
-			if (result.errors) {
-				console.error(result.errors);
-				reject(result.errors);
-			}
+			`).then((result) => {
+				if (result.errors) {
+					console.error(result.errors);
+					reject(result.errors);
+				}
 
-			const edges = result.data.allContentfulPost.edges;
+				const edges = result.data.allContentfulPost.edges;
 
-			const tags = getTags(edges);
-			const categories = getCategories(edges);
+				const tags = getTags(edges);
+				const categories = getCategories(edges);
 
-			// Create posts and pages.
-			_.each(edges, (edge, index) => {
-				const slug = edge.node.slug;
+				// Create posts and pages.
+				_.each(edges, (edge, index) => {
+					const slug = edge.node.slug;
 
-				const previous = index === edges.length - 1 ? null : edges[index + 1].node;
-				const next = index === 0 ? null : edges[index - 1].node;
+					const previous = index === edges.length - 1 ? null : edges[index + 1].node;
+					const next = index === 0 ? null : edges[index - 1].node;
 
-				//	create posts pages
-				createPage({
-					path: `/posts/${slug}/`,
-					component: postTemplate,
-					context: {
-						slug: slug,
-						previous,
-						next,
-					},
+					//	create posts pages
+					createPage({
+						path: `/posts/${slug}/`,
+						component: postTemplate,
+						context: {
+							slug: slug,
+							previous,
+							next
+						}
+					});
 				});
-			});
 
-			// Make tag pages
-			_.uniq(tags).forEach(tag => {
-				createPage({
-					path: `/tags/${tag.slug}/`,
-					component: tagTemplate,
-					context: {
-						slug: tag.slug
-					},
-				})
-			});
+				// Make tag pages
+				_.uniq(tags).forEach((tag) => {
+					createPage({
+						path: `/tags/${tag.slug}/`,
+						component: tagTemplate,
+						context: {
+							slug: tag.slug
+						}
+					});
+				});
 
-			// Make category pages
-			_.uniqBy(categories, 'id').forEach(category => {
-				createPage({
-					path: `/categories/${category.slug}/`,
-					component: categoryTemplate,
-					context: {
-						slug: category.slug
-					},
-				})
-			});
-		}));
+				// Make category pages
+				_.uniqBy(categories, 'id').forEach((category) => {
+					createPage({
+						path: `/categories/${category.slug}/`,
+						component: categoryTemplate,
+						context: {
+							slug: category.slug
+						}
+					});
+				});
+			})
+		);
 	});
 };
+
+//	Netlify cms fix for netlify deploys?
+module.exports.resolvableExtensions = () => [ '.json' ];
