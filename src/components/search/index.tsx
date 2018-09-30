47290ed9a56684c0fe35d04fe02f54997974d58b
Romeo Mihalcea <romeo.mihalcea@gmail.com>
2018-09-30 04:15:28 +0300

    Fixed rss feed

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,12 +1,12 @@
-import React, {Component} from "react";
+import React, {Component} from 'react';
 
 import {debounce} from 'lodash';
-import algoliasearch, {Response} from "algoliasearch";
-import styled from "styled-components";
-import {HTMLDivProps} from "@blueprintjs/core/src/common/props";
-import {graphql, StaticQuery} from "gatsby";
+import algoliasearch, {Response} from 'algoliasearch';
+import styled from 'styled-components';
+import {HTMLDivProps} from '@blueprintjs/core/src/common/props';
+import {getPluginsConfig} from '../../utils/helpers';
 
-import Algolia from "./algolia.svg";
+import Algolia from './algolia.svg';
 
 const Wrapper = styled.div`
     img {
@@ -16,20 +16,12 @@ const Wrapper = styled.div`
 `;
 
 export interface DataProps {
-    site: {
-        siteMetadata: {
-            algolia: {
-                appId: string;
-                indexName: string;
-                searchKey: string;
-            }
-        }
-    }
+
 }
 
 interface Props {
-    onWrite?: () => void;
-    onResults?: (results: Response['hits']) => void;
+	onWrite?: () => void;
+	onResults?: (results: Response['hits']) => void;
 }
 
 interface State {
@@ -37,54 +29,34 @@ interface State {
 }
 
 export default class Search extends Component<Props & HTMLDivProps, State> {
-    render() {
-        const {...props} = this.props;
+	render() {
+		const {...props} = this.props;
 
-        return "Not implemented!"
+		const client = algoliasearch(
+			getPluginsConfig(['search', 'algolia', 'appId']),
+			getPluginsConfig(['search', 'algolia', 'searchKey']),
+		);
+		const index = client.initIndex(getPluginsConfig(['search', 'algolia', 'indexName']));
 
-        // return <StaticQuery
-        //     query={graphql`
-        //         query {
-        //             site {
-        //                 siteMetadata {
-        //                     algolia {
-        //                         appId
-        //                         indexName
-        //                         searchKey
-        //                     }
-        //                 }
-        //             }
-        //         }
-        //     `}
-	   //
-        //     render={(data: DataProps) => {
-        //         const algoliaCfg = data.site.siteMetadata.algolia;
-        //         const client = algoliasearch(algoliaCfg.appId, algoliaCfg.searchKey);
-        //         const index = client.initIndex(algoliaCfg.indexName);
-	   //
-        //         const search = debounce((query: string) => {
-        //             if (this.props.onWrite) {
-        //                 this.props.onWrite();
-        //             }
-	   //
-        //             index.search({query}, (err, content) => {
-        //                 if (this.props.onResults) this.props.onResults(content.hits);
-        //             });
-        //         }, 800).bind(this);
-	   //
-        //         return (
-        //             <Wrapper className="qards-search" {...props}>
-        //                 <input
-        //                     className="bp3-input bp3-large bp3-fill"
-        //                     type="text" placeholder="Search"
-        //                     onChange={(e) => search(e.target.value)}
-        //                 />
-	   //
-        //                 <img src={Algolia} alt={"Search by algolia"}/>
-	   //
-        //             </Wrapper>
-        //         );
-        //     }}
-        // />
-    }
+		const search = debounce((query: string) => {
+			if (this.props.onWrite) {
+				this.props.onWrite();
+			}
+
+			index.search({query}, (err, content) => {
+				if (this.props.onResults) this.props.onResults(content.hits);
+			});
+		}, 800).bind(this);
+
+		return (
+			<Wrapper className="qards-search" {...props}>
+				<input
+					className="bp3-input bp3-large bp3-fill"
+					type="text" placeholder="Search"
+					onChange={(e) => search(e.target.value)}
+				/>
+				<img src={Algolia} alt={'Search by algolia'}/>
+			</Wrapper>
+		);
+	}
 }
