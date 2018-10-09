32718a95a87ed0eeae53ae33668a6dea11f413a0
Romeo Mihalcea <romeo.mihalcea@gmail.com>
2018-10-10 01:00:52 +0300

    Fixes first deploy issues

--- src/app/pages/index.example.tsx
+++ src/app/pages/index.example.tsx
@@ -21,7 +21,7 @@ const Blog = () => {
 				latest: allMarkdownRemark(
 					sort: {fields: [frontmatter___created_at], order: DESC},
 					filter: {
-						fileAbsolutePath: {regex: "//collections/posts//"},
+						fileAbsolutePath: {regex: "//static/content/collections/posts//"},
 						frontmatter: {isPage: {ne: true}}
 					}
 				) {
