3b0e0186b24226967bf1e06a67a653f64d335c28
Romeo Mihalcea <romeo.mihalcea@gmail.com>
2018-11-27 18:36:30 +0200

    Removed fixed height from navbar

--- src/components/navbar/styles.tsx
+++ src/components/navbar/styles.tsx
@@ -9,7 +9,7 @@ export const StyledNavbar = styled(Navbar)`
 	padding: 0 !important;
 	box-shadow: none !important;
 	background: transparent !important;
-	
+	height: auto;
 
 	a[role='button'],
 	button {
