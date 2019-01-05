aeaf5030ec3078f65580fc82b3371e89f55dcf01
Romeo Mihalcea <romeo.mihalcea@gmail.com>
2019-01-05 23:23:47 +0200

    Netlify cms testing

--- testing/cypress/integration/qards/cms/netlify.spec.ts
+++ testing/cypress/integration/qards/cms/netlify.spec.ts
@@ -1,24 +1,40 @@
 /// <reference types="Cypress" />
-
 const chaiColors = require('chai-colors');
 
 chai.use(chaiColors);
 
 Cypress.on('uncaught:exception', (err, runnable) => {
 	// returning false here prevents Cypress from
-	// failing the test
+	// failing the test on exceptions
 	return false;
 });
 
 describe('Test Backend', () => {
-	beforeEach(() => {
-		cy.visit('/admin/#/collections/posts/entries/list-of-supported-cards');
-	});
+	// beforeEach(() => {
+	// 	cy.visit('/admin/#/collections/posts/entries/list-of-supported-cards');
+	// });
 
-	const login = () => {
+	const setSiteUrlAndLogin = () => {
 		cy.viewport(1200, 1200);
 		cy.visit('/admin/');
-		cy.get('input.formControl').clear().type('https://www.qards.io/');
-		cy.get('button.btn').click();
+
+		cy.get('#netlify-identity-widget').then(($iframe) => {
+			const $body = $iframe.contents().find('body');
+			let form = cy.wrap($body[0]);
+
+			form.find('input.formControl').eq(0).clear().type(
+				'https://priceless-kare-fdc4ff.netlify.com', {force: true},
+			);
+			cy.wrap($body[0]).find('button.btn').eq(0).click();
+		});
+
+		cy.get(`iframe`).then(($iframe) => {
+			const $body = $iframe.contents().find('body');
+			cy.wrap($body[0]).get('button').click();
+		});
 	};
+
+	it('should set site url to the cms', () => {
+		setSiteUrlAndLogin();
+	});
 });
