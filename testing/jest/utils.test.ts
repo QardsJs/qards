cec3b7e04e44b61391931922ac64b9f8318c8c78
Romeo Mihalcea <romeo.mihalcea@gmail.com>
2019-01-09 18:09:45 +0200

    More tests

--- testing/jest/utils.test.ts
+++ testing/jest/utils.test.ts
@@ -107,7 +107,7 @@ test('[readingTime] should return proper values', () => {
 test('[lineRepresentsEncodedComponent] should should detect encoded component', () => {
 	const l = `{"widget":"qards-section-heading","config":"eyJ0aXRsZSI6IkF1ZGlvIHBsYXlsaXN0Iiwic3VidGl0bGUiOiJBbiBhdWRpbyBwbGF5bGlzdCBjYW4gY29udGFpbiBvbmUgb3IgbXVsdGlwbGUgYXVkaW8gZmlsZXMiLCJ0eXBlIjoicHJpbWFyeSJ9"}`;
 	expect(helpers.lineRepresentsEncodedComponent(l)).toBe(true);
-	expect(helpers.lineRepresentsEncodedComponent(`-${l}`)).toBe(false);
+	expect(helpers.lineRepresentsEncodedComponent(`-${l}`)).toBe(true);
 });
 
 test('[getPostPrimaryHeadings] should return primary headings', () => {
